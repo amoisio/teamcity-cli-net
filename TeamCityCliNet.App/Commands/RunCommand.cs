c52e694000a8b344b9b48d83d73f8a8c7a780eea
Aleksi Moisio <aleksi.moisio30@gmail.com>
2021-01-24 18:42:43 +0200

    Change run build parameters and add a 'wait' parameter which can be used for waiting on  a build.

--- TeamCityCliNet.App/Commands/RunCommand.cs
+++ TeamCityCliNet.App/Commands/RunCommand.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CliFx;
@@ -20,13 +21,11 @@ namespace TeamCityCliNet.Commands
         [CommandParameter(0, Description = "Build configuration.")]
         public string Configuration { get; set; }
 
-
-
         [CommandOption("number", 'n', Description = "")]
         public string Number { get; set; }
 
         [CommandOption("personal", Description = "")]
-        public bool? Personal { get; set; }
+        public bool Personal { get; set; }
 
         [CommandOption("branch", 'b', Description = "")]
         public string Branch { get; set; }
@@ -35,23 +34,65 @@ namespace TeamCityCliNet.Commands
         public string Comment { get; set; }
 
         [CommandOption("rebuidAll", Description = "")]
-        public bool? RebuildAllDependencies { get; set; }
+        public bool RebuildAllDependencies { get; set; }
 
         [CommandOption("clean", Description = "")]
         public bool? CleanSources { get; set; }
 
         [CommandOption("top", Description = "")]
-        public bool? QueueAtTop { get; set; }
+        public bool QueueAtTop { get; set; }
 
         [CommandOption("parameters", 'p', Description = "")]
         public string[] Parameters { get; set; }
 
+        [CommandOption("wait", 'w', Description="")]
+        public bool Wait { get; set; }
+        
         public async ValueTask ExecuteAsync(IConsole console)
         {
-            console.Output.WriteLine(Parameters);
-            // var config = await _teamCity.BuildTypes.ById(new Id(Configuration)).ConfigureAwait(false);
-            // config.RunBuild(
-            // ItemPrinter.Print(console, build);
+            var parameters = ParseParameters(Parameters);
+            var config = await _teamCity.BuildTypes.ById(new Id(Configuration)).ConfigureAwait(false);
+            var build = await config.RunBuild(
+                parameters,
+                QueueAtTop,
+                CleanSources,
+                RebuildAllDependencies,
+                Comment,
+                Branch,
+                Personal).ConfigureAwait(false);
+
+            ItemPrinter.Print(console, build);
+
+            if (Wait)
+            {
+                var id = build.Id;
+                while(build.State != BuildState.FINISHED)
+                {
+                    int delay = 1000;
+                    await Task.Delay(delay);
+                    build = await _teamCity.Builds.ById(id).ConfigureAwait(false);
+                    console.Output.WriteLine(build.StatusText);
+                }
+                console.Output.WriteLine("Build done!");
+            }
+        }
+
+        private IDictionary<string, string> ParseParameters(string[] values)
+        {
+            if (values == null || values.Length == 0)
+                return null;
+
+            var parameters = new Dictionary<string, string>(values.Length);
+            foreach(var parameter in values)
+            {
+                var components = parameter.Split('=');
+                if (components.Length != 2) 
+                    continue;
+                else
+                    parameters.Add(components[0], components[1]);
+
+            }
+            return parameters;
         }
     }
 }
