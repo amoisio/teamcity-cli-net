dc6c2576f366e00c097ed13ac5b349537d2337ee
Aleksi Moisio <aleksi.moisio30@gmail.com>
2021-01-24 13:21:16 +0200

    Add run command options

--- TeamCityCliNet.App/Commands/RunCommand.cs
+++ TeamCityCliNet.App/Commands/RunCommand.cs
@@ -17,17 +17,41 @@ namespace TeamCityCliNet.Commands
             _teamCity = teamCity;
         }
 
-        [CommandParameter(0, Description = "Username of the user to find.")]
-        public string Type { get; set; }
+        [CommandParameter(0, Description = "Build configuration.")]
+        public string Configuration { get; set; }
+
+
 
         [CommandOption("number", 'n', Description = "")]
         public string Number { get; set; }
 
+        [CommandOption("personal", 'p', Description = "")]
+        public bool? Personal { get; set; }
+
+        [CommandOption("branch", 'b', Description = "")]
+        public string Branch { get; set; }
+
+        [CommandOption("comment", 'c', Description = "")]
+        public string Comment { get; set; }
+
+        [CommandOption("rebuidAll", Description = "")]
+        public bool? RebuildAllDependencies { get; set; }
+
+        [CommandOption("clean", Description = "")]
+        public bool? CleanSources { get; set; }
+
+        [CommandOption("top", Description = "")]
+        public bool? QueueAtTop { get; set; }
+
+        [CommandOption("parameters", 'p', Description = "")]
+        public string[] Parameters { get; set; }
 
         public async ValueTask ExecuteAsync(IConsole console)
         {
-            var build = await _teamCity.Build(new Id(Type), Number).ConfigureAwait(false);
-            ItemPrinter.Print(console, build);
+            console.Output.WriteLine(Parameters);
+            // var config = await _teamCity.BuildTypes.ById(new Id(Configuration)).ConfigureAwait(false);
+            // config.RunBuild(
+            // ItemPrinter.Print(console, build);
         }
     }
 }
